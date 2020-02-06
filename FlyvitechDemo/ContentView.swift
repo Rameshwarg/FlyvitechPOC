54b59c814591c7a19827f4abae62e467d6f41ca2
Ram Gade <RameshwarGade@Discovery.bank>
2020-02-06 17:26:53 +0530

    Initial Commit

---
+++ FlyvitechDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlyvitechDemo
+//
+//  Created by Ram Gade on 2020/02/06.
+//  Copyright © 2020 Ram Gade. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
