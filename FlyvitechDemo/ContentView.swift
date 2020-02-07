4313652797e3ec55dac9ba37eca71a080a3aa069
Ram Gade <RameshwarGade@Discovery.bank>
2020-02-07 11:24:41 +0530

    feat: add initial setup and data fetch

--- FlyvitechDemo/ContentView.swift
+++ FlyvitechDemo/ContentView.swift
@@ -7,15 +7,83 @@
 //
 
 import SwiftUI
+import ObjectMapper
+import struct Kingfisher.KFImage
 
 struct ContentView: View {
+    
+    @ObservedObject var fetcher = MovieFetcher()
+    var refreshControl = UIRefreshControl()
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List(fetcher.movies) { cellData in
+                KFImage(URL(string:cellData.imageHref ?? ""))
+                    .resizable()
+                    .frame(width: 100, height: 100).cornerRadius(50)
+                VStack(alignment: .leading) {
+                    Text(cellData.title ?? "")
+                    Text(cellData.description ?? "").font(.subheadline).colorMultiply(.red)
+                }
+            }.navigationBarTitle(Text(fetcher.hearderTitle))
+        }
+    }
+    
+    func initialize() {
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        //refreshControl.addTarget(self, action: #selector(refresh(sender:)), for: UIControl.Event.valueChanged)
+    }
+    
+}
+
+public class MovieFetcher: ObservableObject {
+    
+    @Published var movies = [Data]()
+    @Published var hearderTitle = ""
+    
+    init(){
+        load()
+    }
+    
+    func load() {
+        let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")!
+        
+        URLSession.shared.dataTask(with: url) {(data,response,error) in
+            do {
+                if let listData = data {
+                    
+                    let utf8Data = String(decoding: listData, as: UTF8.self).data(using: .utf8)
+                    
+                    let json = try? JSONSerialization.jsonObject(with: utf8Data!, options: [])
+                    let report = try Mapper<RootData>().map(JSON: json as! [String : Any])
+                    DispatchQueue.main.async {
+                        self.movies = report.result ?? []
+                        self.hearderTitle = report.headerTitle ?? ""
+                    }
+                }else {
+                    print("No Data")
+                }
+            } catch {
+                print ("Error")
+            }
+        }.resume()
+        
     }
 }
+
+#if DEBUG
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
     }
 }
+#endif
+
+
+
+
+
+
+
---
+++ FlyvitechDemo/Data.swift
@@ -0,0 +1,38 @@
+//
+//  Data.swift
+//  FlyvitechDemo
+//
+//  Created by Ram Gade on 2020/02/06.
+//  Copyright © 2020 Ram Gade. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+
+public struct RootData {
+    let result : [Data]?
+    let headerTitle:String?
+}
+
+extension RootData: ImmutableMappable {
+    public init(map: Map) throws {
+        result = try? map.value("rows")
+        headerTitle = try? map.value("title")
+    }
+}
+
+struct Data:Identifiable {
+    var id = UUID()
+    let title: String?
+    let description: String?
+    let imageHref: String?
+}
+
+extension Data: ImmutableMappable {
+    public init(map: Map) throws {
+        title = try? map.value("title")
+        description = try? map.value("description")
+        imageHref = try? map.value("imageHref")
+    }
+}
